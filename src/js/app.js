3522c1420bcff73fce7687a5a1f647f760b4010c
Ксения Злакоманова <zlakomanovakv@gmail.com>
2024-06-04 13:09:07 +0300

    change ajax request

--- src/js/app.js
+++ src/js/app.js
@@ -69,65 +69,24 @@ $(document).ready(() => {
     const token = "7330003681:AAGKuGqmyBYJmAG2QpBdSftViL_qS09IboU"
     const chatId = "-4238678546"
 
-    let f_name = $('#user-name').val();
-    let f_phone = $('#user-phone').val();
-    let f_age = $('#user-age').val();
-    let text = 'Новая заявка в лагерь:\n' + 'Имя - ' + f_name + '\n' + 'Номер телефона - ' + f_phone + '\n' + 'Возраст ребенка - ' + f_age;
+    // let f_name = $('#user-name').val();
+    // let f_phone = $('#user-phone').val();
+    // let f_age = $('#user-age').val();
+    // let text = 'Новая заявка в лагерь:\n' + 'Имя - ' + f_name + '\n' + 'Номер телефона - ' + f_phone + '\n' + 'Возраст ребенка - ' + f_age;
 
-    sendOrder.on('submit', function () {
+    sendOrder.click(function () {
 
-        //вызываем функцию отправки в Telegram
+        let f_name = $('#user-name').val();
+        let f_phone = $('#user-phone').val();
+        let f_age = $('#user-age').val();
+        let text = 'Новая заявка в лагерь:\n' + 'Имя - ' + f_name + '\n' + 'Номер телефона - ' + f_phone + '\n' + 'Возраст ребенка - ' + f_age;
+
         $.ajax({
             type: "POST",
             url: "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId,
-            // data: "parse_mode=HTML&text=" + encodeURIComponent(text),
-            data: encodeURIComponent(text),
-            // success: function (data) {
-            //
-            // }
-            //     .done(function (msg) {
-            //         if (msg.success) {
-            //             orderGratitude.show();
-            //             orderInfo.hide();
-            //         } else {
-            //             alert('Приносим свои извинения, но возникли какие-то проблемы, свяжитесь, пожалуйста, с нами, любыми другими способами!');
-            //         }
-            //     })
+            data: "parse_mode=HTML&text=" + encodeURIComponent(text),
         });
 
     });
-
-// Предварительно заполняем данные вашего чата и бота в переменных chatid и token
-//     function send_tg(text) {
-//         $.ajax({
-//             type: "POST",
-//             url: "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId,
-//             data: "parse_mode=HTML&text=" + encodeURIComponent(text),
-//         });
-//     }
-
-    // sendOrder.on('click', () => {
-    //     let userName = $('#user-name');
-    //     let userPhone = $('#user-phone');
-    //     let userAge = $('#user-age');
-    //     console.log('sdkfj')
-    //     $.ajax({
-    //         url: '',
-    //         method: 'post',
-    //         dataType: 'json',
-    //         data: {name: userName.val(), phone: userPhone.val(), age: userAge.val()},
-    //         success: function (data) {
-    //
-    //         }
-    //     })
-    //         .done(function (msg) {
-    //             if (msg.success) {
-    //                 orderGratitude.show();
-    //                 orderInfo.hide();
-    //             } else {
-    //                 alert('Приносим свои извинения, но возникли какие-то проблемы, свяжитесь, пожалуйста, с нами, любыми другими способами!');
-    //             }
-    //         });
-    // });
 });
 
